522256b788d70720c899d9810622503874a64cbd
kumailnxtdev <komailnaqvi616@gmail.com>
2023-02-10 18:40:23 +0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    let mut guess: String  = String::new();
+     
+    io::stdin()
+    .read_line(&mut guess)
+    .expect("Failed to read line");
+    
+    // io::stdin(): Stdin
+    //     .read_line(buf: &mut guess): Result<usize, Error>
+    //     .expect(msg: "failed to read line");
+
+        println!("You guessed: {}", guess)
 }
