343f60be58b79fd9609e7c615a98521216bd5746
Kumail Naqvi <komailnaqvi616@gmail.com>
2023-02-13 10:17:03 +0000

    base logic

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    let mut guess: String  = String::new();
-     
-    io::stdin()
-    .read_line(&mut guess)
-    .expect("Failed to read line");
-    
-    // io::stdin(): Stdin
-    //     .read_line(buf: &mut guess): Result<usize, Error>
-    //     .expect(msg: "failed to read line");
+    println!("The secret number is {}.", secret_number);
+    loop {
+        println!("Please input your guess.");
 
-        println!("You guessed: {}", guess)
+        let mut guess: String = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please Type a Number");
+        // io::stdin(): Stdin
+        //     .read_line(buf: &mut guess): Result<usize, Error>
+        //     .expect(msg: "failed to read line");
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small"),
+            Ordering::Greater => println!("Too Big"),
+            Ordering::Equal => {
+                println!("You Win");
+                break;
+            }
+        }
+    }
 }
